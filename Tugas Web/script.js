e71e569a0c6a3b370b9e4583f6d54a47cc1e941b
Winniethep00h <winylakburlawal10@gmail.com>
2025-04-20 23:30:56 +0900

    Delete Tugas Web directory

--- Tugas Web/script.js
+++
@@ -1,9 +0,0 @@
-function login() {
-    alert("Login berhasil (simulasi)!");
-    return false; // Hindari reload
-  }
-  
-  function register() {
-    alert("Registrasi berhasil (simulasi)!");
-    return false;
-  }
